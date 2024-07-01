41fb18645266bcbdc1673b6443bb7d44bc1238de
dinglu888 <dinglu_wh@126.com>
2024-07-01 11:18:10 +0800

    Update run.py

--- run.py
+++ run.py
@@ -8,5 +8,9 @@ app = Flask(__name__)
 def hello_world():
     return '欢迎使用微信云托管========！'
 
+@app.route('/add', methods=['POST', 'GET'])
+def add(a, b):
+    return a+b;
+
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 80)))
