41707724521f37b31cc83d617bc0741320e56e5a
dinglu888 <dinglu_wh@126.com>
2024-07-01 15:57:46 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,16 +1,23 @@
 import os
 
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '欢迎使用微信云托管========！'
+    return '欢迎使用微信云托管========！';
 
-@app.route('/add', methods=['POST', 'GET'])
+@app.route('/getData', methods=['GET'])
+def getData():
+    data = {"name":"xiaoming", "age":"18"}
+    return data;
+
+@app.route('/add', methods=['POST'])
 def add(a, b):
     return a+b;
+
+
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 80)))
