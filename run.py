080c0caa8f4dbd68217fa9d22a317de7a89e7634
dinglu888 <dinglu_wh@126.com>
2024-07-02 18:32:11 +0800

    so

--- run.py
+++ run.py
@@ -37,13 +37,11 @@ def cb_sayhello():						# 无参数的回调函数
   
     solib =  ctypes.CDLL('./libhello.so')   # 加载动态链接库
     
-    func_send_message = solib.send_message
-    # CFUNCTYPE定义方法的签名，第一参数表示方法的返回类型，后面开始编译参数的类型
-    funcStruct =  CFUNCTYPE(None)			
-    solib.send_message(10,funcStruct(cb_sayhello))
+    func_say_hello4 = solib.say_hello4
+    res = func_say_hello4(1,2);
     print("============================")
     
-    return "hello world";
+    return res;
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 80)))
