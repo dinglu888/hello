181a1cb89f466293965d0297d1528c9e1e6dae2b
dinglu888 <dinglu_wh@126.com>
2024-07-03 18:04:10 +0800

    1804

--- run.py
+++ run.py
@@ -29,9 +29,7 @@ def cb_sayhello():
 @app.route('/camera_calibration')
 def camera_calibration():
     lib = ctypes.CDLL('./libgalvanometer_correction.so')
-    func_camera_calibration = lib.camera_calibration  
-
-    return '111111'    
+    func_camera_calibration = lib.camera_calibration    
         
     image_path = './camera_calibration.jpg'
     is_circle = True
@@ -44,33 +42,13 @@ def camera_calibration():
     ls_kernel_cross = 7
     ls_area_max = 500000
     ls_area_min = 1
-    
-    return '22222'
-    
+       
     rect_instance = (0, 0, 1080, 985) 
     rect_array = (ctypes.c_int * 4)(*rect_instance)
     
     return '33333'
 
-    success = func_camera_calibration(
-        image_path.encode('utf-8'),
-        ctypes.cast(rect_array, ctypes.POINTER(ctypes.c_int)),
-        ctypes.c_bool(is_circle),
-        ctypes.c_int(index),
-        ctypes.c_double(area),
-        ctypes.c_double(ls_circle),
-        ctypes.c_double(ls_convex),
-        ctypes.c_double(ls_ineria),
-        ctypes.c_int(ls_kernel),
-        ctypes.c_int(ls_kernel_cross),
-        ctypes.c_int(ls_area_max),
-        ctypes.c_int(ls_area_min)
-    )
-    
-    if success:
-        return 'Camera calibration successful'
-    else:
-        return 'Camera calibration failed'
+
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 80)))
