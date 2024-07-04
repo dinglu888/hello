f7614b59ea50c767e2bebdc98e418499f9774811
dinglu888 <dinglu_wh@126.com>
2024-07-04 17:59:10 +0800

    Update run.py

--- run.py
+++ run.py
@@ -33,6 +33,9 @@ def camera_calibration():
         return 'Library loaded successfully'
     except OSError as e:
         return 'Failed to load library'
-        
+
+
+
+
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 80)))
