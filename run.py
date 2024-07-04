e5b6e5a75076f656701ab6a324e2ea478e73b18d
dinglu888 <dinglu_wh@126.com>
2024-07-04 17:54:17 +0800

    1754

--- run.py
+++ run.py
@@ -28,9 +28,15 @@ def cb_sayhello():
    
 @app.route('/camera_calibration', methods=['POST'])
 def camera_calibration():
-    area = request.json['area'];
-    
-    return  jsonify(area); 
+
+    try:
+        lib = ctypes.CDLL('./libgalvanometer_correction.so')
+        return 'Library loaded successfully'
+    except OSError as e:
+        return 'Failed to load library'
+        
+    #area = request.json['area']; 
+    #return  jsonify(area); 
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 80)))
