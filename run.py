1a95e86fd4baeca73384490bfc59d74f8eb15fad
dinglu888 <dinglu_wh@126.com>
2024-07-05 14:34:15 +0800

    1434

--- run.py
+++ run.py
@@ -28,24 +28,24 @@ def cb_sayhello():
    
 @app.route('/camera_calibration', methods=['POST'])
 def camera_calibration():
-    index = request.json['index'];
-    area = request.json['area'];
+    index = request.json["index"];
+    area = request.json["area"];
     ls_circle = request.json['ls_circle'];
     name = request.json['name'];
     
-    json_str = '{"name": "John", "age": 30, "age1": 20, "city": "New York"}'
+    #json_str = '{"name": "John", "age": 30, "age1": 20, "city": "New York"}'
     #将JSON字符串解析为Python对象
-    data = json.loads(json_str)
-    age = data["age"]
-    age1 = data["age1"]
+    #data = json.loads(json_str)
+    #age = data["age"]
+    #age1 = data["age1"]
     
     solib =  ctypes.CDLL('./libhello.so')   # 加载动态链接库  
     func_say_hello4 = solib.say_hello4
-    res = func_say_hello4(age,age1);  
-    return jsonify(res);
+    res = func_say_hello4(index,area);  
+    #return jsonify(res);
     
-    #str = "%s,%s,%s,%s" %(index, area, ls_circle, name)
-    #return str;
+    str = "%s,%s,%s,%s,%s" %(index, area, ls_circle, name, res)
+    return str;
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 80)))
