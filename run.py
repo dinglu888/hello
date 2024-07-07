4b0c42479d36dddde1b4f4d15a85d33d65c80021
dinglu888 <dinglu_wh@126.com>
2024-07-07 17:48:32 +0800

    Update run.py

--- run.py
+++ run.py
@@ -48,7 +48,7 @@ def camera_calibration():
     index = int(data['index'])
     area = int(data['area'])
     ls_circle = float(data['ls_circle'])
-    name = str(data['name'])
+    #name = str(data['name'])
     
     #json_str = '{"name": "John", "age": 30, "age1": 20, "city": "New York"}'
     #将JSON字符串解析为Python对象
@@ -61,10 +61,10 @@ def camera_calibration():
     res = func_say_hello4(index,area);  
     #return jsonify(res);
     
-    str = "%s,%s,%s,%s,%s" %(index, area, res, ls_circle, name)
+    #str = "%s,%s,%s,%s,%s" %(index, area, res, ls_circle, name)
     #报500错误
     #return json.dumps(str.decode('utf8'));
-    return str;
+    return jsonify(ls_circle);
     
 
 if __name__ == "__main__":
